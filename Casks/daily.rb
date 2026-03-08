0224a0a51ce4777411f2f9f1984fd8c362a42a64
Oleg 👾 <infectedbyjs@gmail.com>
2026-03-09 01:36:38 +1000

    Fix cask audit: remove unnecessary verified

--- Casks/daily.rb
+++ Casks/daily.rb
@@ -2,8 +2,7 @@ cask "daily" do
   version "0.12.2"
   sha256 "77a7f73713ae0b13b4cccee61bdf4fea2c1708b781b7358cc8bb7a50d2dfae8c"
 
-  url "https://github.com/scheron/Daily/releases/download/v#{version}/Daily-#{version}-mac.dmg",
-      verified: "github.com/scheron/Daily/"
+  url "https://github.com/scheron/Daily/releases/download/v#{version}/Daily-#{version}-mac.dmg"
   name "Daily"
   desc "Simple daily planning and note-taking app"
   homepage "https://github.com/scheron/Daily"
